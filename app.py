87a7878410d0532e1cf71917e9feb38c4f037ed9
Mini Project <project@example.com>
2026-03-10 22:33:02 +0530

    Auto-commit: Changes saved at 2026-03-10 22:33:02

--- app.py
+++ app.py
@@ -177,6 +177,6 @@ def predict_crop():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)  # Ensures Flask runs on port 5000
+    app.run(debug=True, host="localhost", port=5000)  # Ensures Flask runs on port 5000
 
 
